9614a5eabf4b2fbfbfdb173652d62ce0b3f22c7a
noomesk <aplazasn@unal.edu.co>
2026-08-03 17:44:12 -0500

    Agregar etiqueta search-filters en index.html para prueba temporal

---
+++ rick-morty-portal/src/components/SearchFilters.js
@@ -0,0 +1,37 @@
+class SearchFilters extends HTMLElement {
+  constructor() {
+    super(); // obligatorio: le dice a HTMLElement que se inicialice primero
+    this.attachShadow({ mode: 'open' }); // crea la cápsula de cristal
+  }
+
+  connectedCallback() {
+    // Este método se ejecuta automáticamente cuando el elemento
+    // ya está insertado en el documento (cuando el navegador lo "monta")
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        .filters-container {
+          display: flex;
+          gap: 10px;
+          padding: 16px;
+        }
+      </style>
+
+      <form class="filters-container">
+        <input type="text" placeholder="Buscar personaje..." />
+        <select>
+          <option value="">Todos</option>
+          <option value="Alive">Alive</option>
+          <option value="Dead">Dead</option>
+          <option value="unknown">Unknown</option>
+        </select>
+        <button type="submit">Buscar</button>
+      </form>
+    `;
+  }
+}
+
+customElements.define('search-filters', SearchFilters);
